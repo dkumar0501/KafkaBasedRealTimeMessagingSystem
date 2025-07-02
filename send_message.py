b3cd2837114a48280d15bb7ffadb7e9a5db086e4
D Kumar <deepkumar0501@gmail.com>
2025-07-03 00:16:50 +0530

    Add files via upload

---
+++ send_message.py
@@ -0,0 +1,11 @@
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+topic = 'juyel-topic'
+for i in range(10):
+    message = f'Hello Kafka {i}'
+    producer.send(topic, value=message.encode('utf-8'))
+    print(f"Sent: {message}")
+
+producer.flush()
