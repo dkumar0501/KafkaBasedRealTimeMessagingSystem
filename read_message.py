778c9a72f8d090adde41587ddb197bc0a3380bd0
D Kumar <deepkumar0501@gmail.com>
2025-07-26 10:52:28 +0530

    Update read_message.py

--- read_message.py
+++ read_message.py
@@ -2,7 +2,7 @@ from kafka import KafkaConsumer
 
 consumer = KafkaConsumer(
     'juyel-topic',
-    bootstrap_servers='localhost:9092',
+    bootstrap_servers='localhost:9092', 
     auto_offset_reset='earliest',
     enable_auto_commit=True,
     group_id='my-group'
