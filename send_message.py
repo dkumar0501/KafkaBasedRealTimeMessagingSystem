1379adcf323f235b45f7712bbf84677cfc4178d9
D Kumar <deepkumar0501@gmail.com>
2025-07-29 12:33:26 +0530

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -2,7 +2,7 @@ from kafka import KafkaProducer
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
-topic = 'deep-topic'
+topic = 'deep-topic' 
 for i in range(10): 
     message = f'Hello Kafka {i}'
     producer.send(topic, value=message.encode('utf-8'))
