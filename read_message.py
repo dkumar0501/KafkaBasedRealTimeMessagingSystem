e0ae2fac437401477f25050c94612dcdd3dc7ce4
D Kumar <deepkumar0501@gmail.com>
2025-08-07 22:40:08 +0530

    Update read_message.py

--- read_message.py
+++ read_message.py
@@ -1,7 +1,7 @@
 from kafka import KafkaConsumer
  
 consumer = KafkaConsumer(
-    'juyel-topic', 
+    'juyel-topic',  
     bootstrap_servers='localhost:9092',  
     auto_offset_reset='earliest',
     enable_auto_commit=True,
@@ -11,3 +11,4 @@ consumer = KafkaConsumer(
 print("Reading messages from Kafka:") 
 for message in consumer:
     print(f"Received: {message.value.decode('utf-8')}")
+
