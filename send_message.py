b82774605e0865731a287f9eb5d851d88c5806c4
D Kumar <deepkumar0501@gmail.com>
2025-08-20 20:06:37 +0530

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -7,12 +7,13 @@ for i in range(10):
     message = f'Hello Kafka {i}'  
     producer.send(topic, value=message.encode('utf-8'))
     print(f"Sent: {message}")  
- 
+  
 producer.flush() 
   
   
 
  
+
 
 
 
